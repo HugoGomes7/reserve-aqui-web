a4ebe273a1a6fcdf19da8109b9f0964a56f15bd7
Hugo Gomes <hugorgomes10@gmail.com>
2023-01-06 11:03:10 -0300

    feat: active link

--- script.js
+++ script.js
@@ -55,20 +55,46 @@ sr.reveal('#footer .brand, #footer .social', {
   duration: 600,
 })
 
+const header = document.querySelector('#header');
+const navHeight = header.offsetHeight;
+
 function changeHeaderWhenScroll() {
-  const header = document.querySelector('#header');
-  const navHeight = header.offsetHeight;
   window.scrollY >= navHeight ? header.classList.add('scroll') : header.classList.remove('scroll')
 }
 
+const backToTopButton = document.querySelector('.back-to-top')
+
 function backToTop() {
-  const backToTopButton = document.querySelector('.back-to-top')
   window.addEventListener('scroll', function () {
     window.scrollY >= 100 ? backToTopButton.classList.add('show') : backToTopButton.classList.remove('show')
   })
 }
 
+const sections = document.querySelectorAll('main section[id]')
+
+function activateMenuAtCurrentSection() {
+  const limit = window.pageYOffset + (window.innerHeight / 8) * 4
+
+  for (const section of sections) {
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.offsetHeight
+    const sectionId = section.getAttribute('id')
+
+    const limitStart = limit >= sectionTop
+    const limitEnd = limit <= sectionTop + sectionHeight
+
+    if (limitStart && limitEnd) {
+      document.querySelector('nav ul li a[href*=' + sectionId + ']').classList.add('active')
+    }
+    else {
+      document.querySelector('nav ul li a[href*=' + sectionId + ']').classList.remove('active')
+    }
+  }
+
+}
+
 window.addEventListener('scroll', function () {
   changeHeaderWhenScroll();
   backToTop();
+  activateMenuAtCurrentSection();
 })
